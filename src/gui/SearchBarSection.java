4690f555d9373a23b7d9f4b0adb5152e4d607023
Simon-Dao <SimonNDao12>
2020-03-27 15:46:43 -0700

    search bar can now show results based on what the user typed instead of the whole list

--- src/gui/SearchBarSection.java
+++ src/gui/SearchBarSection.java
@@ -3,9 +3,10 @@ package gui;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import popup.TextFields;
+
+import java.util.ArrayList;
 
 //TODO make the program search depending on what the input starts with
 
@@ -13,124 +14,51 @@ public class SearchBarSection {
 
     public TextField search;
 
+    private String[] suggestions = null;
+
     public SearchBarSection(Pane pane) {
         search = new TextField();
         search.setPromptText("search for others!");
         search.setLayoutX(10);
         search.setLayoutY(26);
-        pane.getChildren().add(search);
-
-        //temp
-        String[] a = {"EleanorBowman2050",
-        "EmilyHunt1509",
-                "LincolnSwanson2879",
-                "JacobMalone706",
-                "Harpernull1759",
-                "Noranull4111",
-                "LillianMorrison3176",
-                "MilaHartman4494",
-                "WyattMalone2695",
-                "JoshuaLeigh743",
-                "JohnLove421",
-                "LoganAyala4655",
-                "AbigailPeters2203",
-                "JohnMcgrath2857",
-                "Chloenull4392",
-                "AmeliaCartwright3298",
-                "ElijahPerkins3519",
-                "JamesDawson457",
-                "MadisonMalone1498",
-                "AuroraHammond3674",
-                "IsabellaClarke317",
-                "NoahHunt1674",
-                "GraceHunt4875",
-                "JacksonWood3142",
-                "ClaireWilliamson2684",
-                "NoraMorrison2943",
-                "JackHolmes1311",
-                "HazelBryant1880",
-                "ScarlettBass4016",
-                "ElizabethBlaese3312",
-                "GraceHartman2694",
-                "AvaSchneider2819",
-                "NoraLloyd242",
-                "VioletGeorge1618",
-                "EthanDoyle485",
-                "WilliamBass1673",
-                "DavidFletcher4559",
-                "LeviCarroll3507",
-                "MichaelHammond4209",
-                "JamesButler4920",
-                "BenjaminMcgrath2622",
-                "Sofianull1352",
-                "StellaCarter4459",
-                "JamesDoyle3688",
-                "ZoeyHartman3883",
-                "AsherClarke4150",
-                "CarterWheeler293",
-                "CharlotteHammond2122",
-                "SofiaWilliams4656",
-                "PenelopeCraig929",
-                "ChristopherReyes3058",
-                "SkylarCraig3127",
-                "GraceDouglas2555",
-                "AnthonyHorne2300",
-                "SkylarLloyd4229",
-                "Lukenull4332",
-                "SavannahDavis4546",
-                "EmmaSawyer4735",
-                "Elijahnull3385",
-                "HannahFletcher4166",
-                "VictoriaKnowles2818",
-                "MasonHamilton958",
-                "BrooklynRodriguez2094",
-                "HenryHorne1647",
-                "MatthewDavis2447",
-                "HazelRodriguez4667",
-                "JacksonHardy1356",
-                "SamuelClarke76",
-                "ScarlettBass2597",
-                "JohnLove3522",
-                "EllaLeigh887",
-                "HenryBass696",
-                "SavannahHartman4189",
-                "LiamFlores4085",
-                "ClaireRodriguez3195",
-                "StellaCartwright4690",
-                "GraceCartwright2275",
-                "HenryByrne260",
-                "GraceHorton3035",
-                "EvelynMorrison4808",
-                "SophiaKnowles2268",
-                "AriaGreene324",
-                "OwenHartman3842",
-                "IsabellaWood206",
-                "StellaHampton3876",
-                "Ryannull3256",
-                "AnthonyHamilton1034",
-                "Amelianull2942",
-                "Clairenull1501",
-                "LincolnHarper2712",
-                "ElizabethDouglas1283",
-                "LillianHammond396",
-                "Natalienull4251",
-                "MasonPark509",
-                "PenelopeWheeler1420",
-                "LiamMitchell4077",
-                "JoshuaCarter1740",
-                "AndrewPerkins525",
-                "NoraBass478",
-                "JaxonReyes589"};
-
-        TextFields.bindAutoCompletion(search,a);
 
         EventHandler searchBarEvent = new EventHandler() {
             @Override
             public void handle(Event event) {
-                System.out.println(search.getText());
+
+                int suggestionCount = 0;
+                ArrayList<Integer> indexes = new ArrayList<>();
+
+                for (int i = 0; i < Test.users.length; i++) {
+                    if(Test.users[i].toLowerCase().startsWith(search.getText())) {
+                        indexes.add(i);
+                        suggestionCount++;
+                    }
+                }
+
+                suggestions = new String[suggestionCount];
+
+                for (int i = 0; i < suggestions.length; i++) {
+                   suggestions[i] = Test.users[indexes.get(i)];
+                   //System.out.println(suggestions[i]);
+                }
+                System.out.println();
+                for (String j: suggestions) {
+                    System.out.println(j);
+                }
+
+                TextFields.bindAutoCompletion(search,suggestions);
+
             }
+
         };
 
-        search.addEventFilter(KeyEvent.KEY_RELEASED, searchBarEvent);
+       //search.addEventFilter(KeyEvent.KEY_RELEASED, searchBarEvent);
+
+       //TextFields.bindAutoCompletion(search,suggestions);
+        String[] test = {"by","hi","try","guy","aasd"};
+
+        TextFields.bindAutoCompletion(search,test);
+        pane.getChildren().add(search);
     }
 }
